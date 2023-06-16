bfd19ce61bd66a370195bd1eee854e6b03034486
ahmed_elabid44 <ahmedelabide@gmail.com>
2023-06-16 12:18:23 -0700

    0posneg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,11 +21,11 @@ int main(void)
 	}
 	else if (n<0)
 	{
-	printf ("%d is negative ",n);
+	printf ("%d is negative",n);
 	}
 	else
 	{
-	printf ("%d is positive ",n);
+	printf ("%d is positive",n);
 	}
 	    
 	
