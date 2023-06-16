f4e3f4904f3a837ad5947b0c40c41556c7348b32
ahmed_elabid44 <ahmedelabide@gmail.com>
2023-06-16 12:39:49 -0700

    0posneg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,15 +13,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if(n == 0)
 {
- printf ("%d is zero\n",n); 
+ printf("%d is zero\n",n); 
 }
-else if (n < 0)
+else if(n < 0)
 {
-printf ("%d is negative\n",n);
+printf("%d is negative\n",n);
 }
 else
 {
-printf ("%d is positive\n",n);
+printf("%d is positive\n",n);
 }
-return (0);
+return(0);
 }
