2fdb8c83398de7eccaec3351b4a160e38e4f1074
ahmed_elabid44 <ahmedelabide@gmail.com>
2023-06-16 12:09:09 -0700

    0posneg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-/*#include <stdio.h>*/
+#include <stdio.h>
 /* more headers goes there */
 /** main - will assign a random number to the variable
  *  n each time it is executed. Complete the source code
