0e6c43d41c06d28804d614deeb731fea0a2064e9
ahmed_elabid44 <ahmedelabide@gmail.com>
2023-06-16 12:31:20 -0700

    0posneg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,11 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 
-/** main - will assign a random number to the variable
- *  n each time it is executed. Complete the source code
- *   in order to print whether the number stored in the
- *   variable n is positive or negative.
-/*   return : 0*/
+/** 
+ * main - will assign a random number negative ,positive and zero
+ * Return: Always 0 (Success)
+ */ 
 
 
 int main(void)
