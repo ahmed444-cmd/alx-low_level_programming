15d1ed7e619cbcbbff57b66fd12360b526daa84b
ahmed_elabid44 <ahmedelabide@gmail.com>
2023-06-22 12:29:43 -0700

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,13 @@
 
 /**
  * _isupper - uppercase letters
- * @ch: char to check
+ * @c: char to check
  * Return: 0 or 1
  */
 
-int _isupper(int ch)
+int _isupper(int c)
 {
-	if (ch >= 'A' && ch <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
