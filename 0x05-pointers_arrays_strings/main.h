8b0933244838c587ff28512787aea8893a41f516
ahmed_elabid44 <ahmedelabide@gmail.com>
2023-06-26 12:02:37 -0700

    task1

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ main.h
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stddef.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -19,4 +20,3 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
 #endif
-
