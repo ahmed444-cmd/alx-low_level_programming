4266936ecb412ba6c415b5bedba43b80b7475126
ahmed <ahmedelabide@gmail.com>
2023-11-23 10:17:00 -0800

    task4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * clear_bit - program that transform the value of a bit to 0 with a known index
+ * @n: value of the bit
+ * @index: index
+ * Return: 1 if success, -1 if error exists
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(n) * 8)
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
+}
