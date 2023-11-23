0591502821d19e5bc99b175515a5567a52108eb9
ahmed <ahmedelabide@gmail.com>
2023-11-23 10:01:01 -0800

    task2

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _pow - function that calculates with (base& power)
+ * @base: base of exponent
+ * @power: power of exponent
+ * Return: value of (base& power)
+*/
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+	unsigned long int num;
+	unsigned int a;
+
+	num = 1;
+	for (a = 1; a <= power; a++)
+		num *= base;
+	return (num);
+}
+
+/**
+ * print_binary - writes a num in binary
+ * @n: numb to write
+ * Return: void
+*/
+void print_binary(unsigned long int n)
+{
+	unsigned long int divisor, check;
+	char flag;
+
+	flag = 0;
+	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (divisor != 0)
+	{
+		check = n & divisor;
+		if (check == divisor)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag == 1 || divisor == 1)
+		{
+			_putchar('0');
+		}
+		divisor >>= 1;
+	}
+}
+
