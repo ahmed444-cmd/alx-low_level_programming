d3cc156dba901d99d04f2018c441197aacd583fb
ahmed <ahmedelabide@gmail.com>
2023-11-23 09:50:15 -0800

    task1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,47 +1,30 @@
 #include "main.h"
 
 /**
- * _pow - function calculates (basepower)
- * @base: base of the exp
- * @power: power of the exp
- * Return: value of (base ^ power)
-*/
-unsigned long int _pow(unsigned int base, unsigned int power)
-{
-	unsigned long int num;
-	unsigned int a;
-
-	num = 1;
-	for (a = 1; a <= power; a++)
-		num *= base;
-	return (num);
-}
-
-/**
- * print_binary - prints a number in binary notation
- * @n: number to print
+ * binary_to_uint - func that converts a base2 number to an unsigned int.
+ * @b: pointer to a string containing a base2 number
  *
- * Return: void
- */
-void print_binary(unsigned long int n)
-{
-	unsigned long int divisor, check;
-	char flag;
+ * Return: unsigned int with decimal value of base2 number, or 0 if error
+*/
 
-	flag = 0;
-	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
-	while (divisor != 0)
+unsigned int binary_to_uint(const char *b)
+{
+	int a;
+	unsigned int num;
+
+	num = 0;
+	if (!b)
+		return (0);
+	for (a = 0; b[a] != '\0'; a++)
 	{
-		check = n & divisor;
-		if (check == divisor)
-		{
-			flag = 1;
-			_putchar('1');
-		}
-		else if (flag == 1 || divisor == 1)
-		{
-			_putchar('0');
-		}
-		divisor >>= 1;
+		if (b[a] != '0' && b[a] != '1')
+			return (0);
 	}
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		num <<= 1;
+		if (b[a] == '1')
+			num += 1;
+	}
+	return (num);
 }
--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c in stdout
- * @c: The printable char
+ * _putchar - puts the char c in stdout
+ * @c: The char to print
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set .
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
