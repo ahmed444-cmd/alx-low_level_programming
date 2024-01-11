0177cf71c0bab98826a2b4f09a7e4d44712f19a2
ahmed <ahmedelabide@gmail.com>
2024-01-11 13:38:47 -0800

    first half

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - show the elems of a
+ * dlistint_t list
+ *
+ * @h: list's head
+ * Return: numb of nds
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count;
+
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
